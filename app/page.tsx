351e613bf223423fb5f1af11c7ce001131f12eef
wtoink <exp6cabal@gmail.com>
2025-11-11 03:39:03 +0700

    Change miniapp name to Ghost

--- app/page.tsx
+++ app/page.tsx
@@ -93,8 +93,7 @@ export default function Home() {
           <h1 className={styles.title}>Mint Ghost NFT</h1>
           
           <p className={styles.subtitle}>
-             Hey {context?.user?.displayName || "there"}, Get early access and be the first to experience the future of<br />
-            crypto marketing strategy.
+             Hey {context?.user?.displayName || "there"}, A ghost NFT created for the Farcaster community. It does not guarantee utility for the NFT, but owning the NFT allows for the possibility of earning unlimited incentives in the future. Minting means supporting Ghost, and Ghost will do its best..
           </p>
 
           <form onSubmit={handleSubmit} className={styles.form}>
